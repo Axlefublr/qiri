c4db8d2caa88e97fcc28d5f5c1d624c389558a73
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2026-01-14 19:43:46 +0800

    fish(injt): `na` is also supported

--- fish/injections.scm
+++ fish/injections.scm
@@ -7,7 +7,7 @@
   (#set! injection.language "jq"))
 
 (command
-  name: (word) @_command (#eq? @_command "nu")
+  name: (word) @_command (#any-of? @_command "nu" "na")
   argument: (word) @_flag (#match? @_flag "^-.*c$")
   argument: [(single_quote_string) (double_quote_string)] @injection.content
   (#set! injection.language "nu")
